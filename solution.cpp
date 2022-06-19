6105c2bb5c14cc7830cacc45ae92c54e88c0adb0
Steven Lin <steven_lx@outlook.com>
2022-06-19 11:55:51 -0700

    add more comments

--- solution.cpp
+++ solution.cpp
@@ -14,12 +14,11 @@ using namespace std;
 /*takeaways
   - DP
     - decode 1 or 2 digits at a time
-    - dp[i] = dp[i-2] if i is valid.
-      - we add s[i] to s[0..i-2] (single digit case)
-      - we are evaluating the string s[0..i-2,i]
-    - dp[i] = dp[i-1] if s[i-1,i] is valid.
-      - we add s[i] to s[0..i-1] (two digit case)
-      - we are evaluating the string s[0..i]
+    - dp[i] = dp[i-1] if i is valid.
+      - (...)-(s[i]) (single digit case)
+
+    - dp[i] = dp[i-2] if s[i-1,i] is valid.
+      - (...)-(s[i-1]s[i]) (two digit case)
     - in implemenation we use two variables
       to remember dp[i-2] and dp[i-1]
 
@@ -35,30 +34,25 @@ int Solution::numDecodings(string s)
     return 1;
 
   /*
-     - prevprev
-       - dp[i-2]
-     - prev
-       - dp[i-1]
      - consider the case of "29"
-       - for "2" there is no dp[0-2]=dp[-2].
-         - that's why we need to set prevprev=1 to
-           the the ball rolling
+       - for "2" there is no dp[0-1]=dp[-1].
+         - that's why dp_minus_1 = 1 initiaily
          - dp[0] = 1
        - for "9"
-         - 9 is valid but there is no dp[1-2]=dp[-1].
-           - that's why we need to set prevprev=1 to
-             the the ball rolling
-           - dp[1] = 1
-         - 29 is not valid so we don't add dp[1-1]=dp[0]
+         - 9 is valid
+           - dp[1] = dp[1-1] = dp[0] = 0
+           - we decode the string as 2-9
+         - 29 is not valid so we don't add dp[1-2]
        - and indeed we only have one way to decode the
          string which is 2-9
   */
-  auto prevprev = 1, prev = 1;
+  auto dp_minus_1 = 1, dp_minus_2 = 1;
   for (auto i = 1; i < s.size(); i++)
   {
     /* for example "200"
        - i = 2
        - s[2] = "0", s[1..2] = "00" neither is valid
+       - 2-00 or 20-0
     */
     if (!_isValid(s[i]) && !_isValid(s[i - 1], s[i]))
       return 0;
@@ -69,21 +63,20 @@ int Solution::numDecodings(string s)
     */
 
     auto w = 0;
-    /* carry over dp[i-2]
-     */
-    if (_isValid(s[i]))
-      w += prevprev;
     /* carry over dp[i-1]
      */
+    if (_isValid(s[i]))
+      w += dp_minus_1;
+    /* carry over dp[i-2]
+     */
     if (_isValid(s[i - 1], s[i]))
-      w += prev;
+      w += dp_minus_2;
 
-    /* w records what you have approved */
-    prevprev = prev;
-    prev = w;
+    dp_minus_2 = dp_minus_1;
+    dp_minus_1 = w;
   }
 
-  return prev;
+  return dp_minus_1;
 }
 
 bool Solution::_isValid(char c)
